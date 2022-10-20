79aa572fed51e6b974fcb9286fd8bd3fe87122dc
sloffilip <113590927+sloffilip@users.noreply.github.com>
2022-10-20 21:01:40 +0200

    Úkoly Python Seznamy

---
+++ Python Seznamy/Fibonacci.py
@@ -0,0 +1,11 @@
+konechod = int(input("Zadejte koncovou hodnotu."))
+
+x = 0
+y = 1
+while y <= konechod:
+    print(x, " ", end="")
+    print(y, " ", end="")
+    x=x+y
+    y=y+x
+if x<=konechod:
+    print(x)
---
+++ Python Seznamy/Rotace.py
@@ -0,0 +1,22 @@
+l1 = [1,2,3,4]
+print("Původní seznam: " + str(l1))
+
+otazka = int(input("O kolik chcete seznam posunout?"))
+otazka2 = input("A jakým směrem?")
+
+if otazka == 1 and otazka2 == "Doleva":
+    l1 = l1[1:] + l1[:1]
+    print("Levá rotace seznamu o 1 : " + str(l1))
+
+if otazka == 1 and otazka2 == "Doprava":
+    l1 = l1[-1:] + l1[:-1]
+    print("Pravá rotace seznamu o 1 : " + str(l1))
+
+if otazka == 2 and otazka2 == "Doprava":
+    l1 = l1[2:] + l1[:2]
+    print("Pravá rotace seznamu o 2 : " + str(l1))
+
+
+if otazka == 2 and otazka2 == "Doleva":
+    l1 = l1[-2:] + l1[:-2]
+    print("Levá rotace seznamu o 2 : " + str(l1))
